9336c47ad2c6549f7e9e0c5072f48094a3c695ad
Prachi pandey <129525113+Prachi7905@users.noreply.github.com>
2023-07-19 00:26:23 +0530

    Add files via upload
    convert temperature form Celsius to Fahrenheit and Fahrenheit to Celsius.

---
+++ TempConverter.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class TempConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("Temperature Converter");
+            System.out.println("---------------------");
+            System.out.println("1. Fahrenheit to Celsius");
+            System.out.println("2. Celsius to Fahrenheit");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    fahrenheitToCelsius(scanner);
+                    break;
+                case 2:
+                    celsiusToFahrenheit(scanner);
+                    break;
+                case 0:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice! Please try again.");
+            }
+        } while (choice != 0);
+
+        scanner.close();
+    }
+
+    public static void fahrenheitToCelsius(Scanner scanner) {
+        System.out.print("Enter temperature in Fahrenheit: ");
+        double fahrenheit = scanner.nextDouble();
+        double celsius = (fahrenheit - 32) * 5 / 9;
+        System.out.println("Temperature in Celsius: " + celsius);
+    }
+
+    public static void celsiusToFahrenheit(Scanner scanner) {
+        System.out.print("Enter temperature in Celsius: ");
+        double celsius = scanner.nextDouble();
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        System.out.println("Temperature in Fahrenheit: " + fahrenheit);
+    }
+}
